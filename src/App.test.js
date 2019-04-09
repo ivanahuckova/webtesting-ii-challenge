bc30b633cde37cb484dd9e9c19233852e28d5117
Ivana Huckova <ivana.huckova@gmail.com>
2019-04-09 15:42:34 +0200

    Create tests for dashboard component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as rtl from 'react-testing-library';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+afterEach(rtl.cleanup);
+
+describe('App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  // it('returns +1 when clicked on foul button and strikes < 2', async () => {
+  //   const wrap = rtl.render(<App />);
+  //   rtl.fireEvent.click(wrap.getByText(/foul/i));
+  //   const strike = await wrap.queryByTestId('strikes');
+  //   console.log(strike);
+  //   expect(strike).toContain(1);
+  // });
 });
--- src/Components/Display.js
+++ src/Components/Display.js
@@ -3,8 +3,12 @@ import React from 'react';
 export default function Display(props) {
   return (
     <div>
-      <div>Count balls: {props.balls}</div>
-      <div>Count strikes: {props.strikes}</div>
+      <div>
+        Count balls: <span data-testid="balls">{props.balls}</span>
+      </div>
+      <div>
+        Count strikes: <span data-testid="strikes">{props.strikes}</span>
+      </div>
     </div>
   );
 }
